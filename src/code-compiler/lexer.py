eac9338d3c918f694b66d2810b1a1c31a27a04a0
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2023-06-06 23:10:44 -0300

    feat: Defines Lexer class and schemas for it.

---
+++ src/code-compiler/lexer.py
@@ -0,0 +1,50 @@
+from typing import Dict, List
+
+from pydantic import BaseModel
+
+
+class SymbolTableEntry(BaseModel):
+    Lines: List[int]
+
+
+class ReservedWords(BaseModel):
+    words: List[str]
+
+
+class Tokens(BaseModel):
+    reserved_words: List[str]
+
+    DEF: str = r"def"
+    IDENT: str = r"[A-Za-z]+[A-Za-z0-9]*"
+    LEFT_PAREN: str = r"\("
+    RIGHT_PAREN: str = r"\)"
+    LEFT_BRACE: str = r"\{"
+    RIGHT_BRACE: str = r"\}"
+    FLOAT: str = r"\d+\.\d+"
+    INT: str = r"\d+"
+    STRING: str = r'"([\w\d]|[^"])*"'
+    SEMICOLON: str = r";"
+    IGNORE: str = r"\t "
+
+
+class Lexer:
+    def __init__(self) -> None:
+        self._symbol_table: Dict[str, SymbolTableEntry] = {}
+        self.reserved_words = ReservedWords(
+            words=[
+                "NEW",
+                "INT",
+                "FLOAT",
+                "STRING",
+                "NULL",
+                "IF",
+                "ELSE",
+                "FOR",
+                "BREAK",
+                "DEF",
+                "RETURN",
+                "READ",
+                "PRINT",
+            ]
+        )
+        self.tokens = Tokens(reserved_words=self.reserved_words.words)
