452792ea2115d1ce873bba91837ebd4f3e0d9935
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2023-06-25 17:49:57 -0300

    feat: WIP Adds more tokens and creates rule methods

--- src/code_compiler/lexer.py
+++ src/code_compiler/lexer.py
@@ -1,5 +1,5 @@
 from textwrap import dedent
-from typing import Dict, List
+from typing import Dict, List, Any
 
 from ply import lex  # type: ignore
 from pydantic import BaseModel
@@ -11,6 +11,8 @@ class SymbolTableEntry(BaseModel):
 
 class Tokens(BaseModel):
     t_DEF: str = r"def"
+    t_BREAKLINE: str = r"\n "
+    t_WHITESPACE: str = r"\s+"
     t_IDENT: str = r"[A-Za-z]+[A-Za-z0-9]*"
     t_LEFT_PAREN: str = r"\("
     t_RIGHT_PAREN: str = r"\)"
@@ -21,6 +23,33 @@ class Tokens(BaseModel):
     t_STRING: str = r'"([\w\d]|[^"])*"'
     t_SEMICOLON: str = r";"
     t_IGNORE: str = r"\t "
+    t_BREAK: str = r"break"
+    t_LEFT_BRACKET: str = r"\["
+    t_RIGHT_BRACKET: str = r"\]"
+    t_INT_CONSTANT: str = r"[0-9]+"
+    t_COMMA: str = r","
+    t_ASSIGN: str = r"="
+    t_PRINT: str = r"print"
+    t_READ: str = r"read"
+    t_RETURN: str = r"return"
+    t_IF: str = r"if"
+    t_ELSE: str = r"else"
+    t_FOR: str = r"for"
+    t_NEW: str = r"new"
+    t_GREATER: str = r">"
+    t_LESS: str = r"<"
+    t_GREATER_EQUAL: str = r">="
+    t_LESS_EQUAL: str = r"<="
+    t_EQUAL: str = r"=="
+    t_NOT_EQUAL: str = r"!="
+    t_PLUS: str = r"\+"
+    t_MINUS: str = r"-"
+    t_MULTIPLY: str = r"\*"
+    t_DIVIDE: str = r"/"
+    t_MODULO: str = r"%"
+    t_FLOAT_CONSTANT: str = r"[0-9]*\.[0-9]+"
+    t_STRING_CONSTANT: str = r'"[^"]*"'
+    t_NULL: str = r"null"
 
 
 class Lexer:
@@ -29,19 +58,6 @@ class Lexer:
 
         self._symbol_table: Dict[str, SymbolTableEntry] = {}
         self.reserved_words = [
-            "NEW",
-            "INT",
-            "FLOAT",
-            "STRING",
-            "NULL",
-            "IF",
-            "ELSE",
-            "FOR",
-            "BREAK",
-            "DEF",
-            "RETURN",
-            "READ",
-            "PRINT",
         ]
 
         self.tokens_model = Tokens(reserved_words=self.reserved_words)
@@ -52,7 +68,7 @@ class Lexer:
         for attr_name, attr_value in self.tokens_model.__dict__.items():
             if attr_name.startswith("t_"):
                 setattr(self, attr_name, attr_value)
-        print('setup ok')
+        self.line_number = 1
 
     def get_tokens_model(self) -> Tokens:
         return self.tokens_model
@@ -82,3 +98,17 @@ class Lexer:
             """
             )
         )
+
+    def t_IDENT(self, t) -> Any:
+        r"[A-Za-z]+[A-Za-z0-9]*"
+        t.type = self.reserved_words.get(t.value, "IDENT")  # Check if the identifier is a reserved word
+        # Update symbol table
+        if t.value not in self._symbol_table:
+            self._symbol_table[t.value] = SymbolTableEntry(Lines=[self.line_number])
+        else:
+            self._symbol_table[t.value].Lines.append(self.line_number)
+        return t
+
+    def t_BREAKLINE(self, t) -> Any:
+        r"\n"
+        self.line_number += 1
