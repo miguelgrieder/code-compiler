d735813ab477aea4ead7444eda70ffe76a48f02a
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2023-06-25 15:40:44 -0300

    style: Formats and adds noqa to print

--- bin/run_parser.py
+++ bin/run_parser.py
@@ -1,14 +1,16 @@
-import sys
 import os
+import sys
 
 from code_compiler import parser, utils
 
-if len(sys.argv) > 1 and sys.argv[1].startswith('FILE='):
+if len(sys.argv) > 1 and sys.argv[1].startswith("FILE="):
     provided_argv = sys.argv[1][5:]
-elif 'FILE' in os.environ:
-    provided_argv = os.environ['FILE']
+elif "FILE" in os.environ:
+    provided_argv = os.environ["FILE"]
 else:
-    print("Error: No file path provided! Pass it as FILE= argument or in the environment.", flush=True)
+    print(  # noqa: T201
+        "Error: No file path provided! Pass it as FILE= argument or in the environment.", flush=True
+    )
     sys.exit(1)
 
 source_code = utils.read_file_content(file_path=provided_argv)
