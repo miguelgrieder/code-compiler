4a34cbacab02fc28dc7c809448f94c50df73daf2
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2023-06-25 22:48:35 -0300

    refactor: Deletes not used get_char_position

--- src/code_compiler/utils.py
+++ src/code_compiler/utils.py
@@ -10,9 +10,3 @@ def read_file_content(file_path: str) -> str:
         sys.exit(1)
     else:
         return file_content
-
-
-def get_char_position(text: str, line: int, char: str) -> int:
-    character_line = text.splitlines()[line - 1]
-    position = character_line.index(char) + 1
-    return position
